08c1892806eb468cf29d677ecf49d1f6b89e2589
Daniel Fraire <sorteosfraire@gmail.com>
2024-11-23 03:10:43 -0700

    Button added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useState } from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
+  const [buttonColor, setButtonColor] = useState('blue');
+
+  const handleButtonClick = () => {
+    setButtonColor(buttonColor === 'blue' ? 'red' : 'blue');
+  };
+
   return (
     <div className="container">
       <Head>
@@ -15,9 +22,15 @@ export default function Home() {
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
+        <button
+          style={{ backgroundColor: buttonColor, color: 'white' }}
+          onClick={handleButtonClick}
+        >
+          Click me
+        </button>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
